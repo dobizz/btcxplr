c3d78a739ea0acbfc022009436c065a24f361c90
Ronnie Villanueva <ronnie.code@outlook.com>
2020-11-16 13:43:18 +0800

    Updated views.py and html templates
    Modified get_txn function to address some bugs encountered when viewing the genesis block transaction.
    
    Added build_genesis_txn to create transaction data from the genesis block data.

--- btcxplr/views.py
+++ btcxplr/views.py
@@ -96,7 +96,13 @@ def get_block(blockid):
 
 @app.route('/txn/<string:txid>', methods=['GET', 'POST'])
 def get_txn(txid):
-    data = rpc.getrawtransaction(txid, verbose=True)
+    # special case for genesis block
+    if txid == '4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b':
+        data=build_genesis_txn()
+
+    else:
+        data = rpc.getrawtransaction(txid, verbose=True)
+
     try:
         data['height'] = rpc.getblock(data['blockhash'])['height']
     except:
@@ -105,15 +111,16 @@ def get_txn(txid):
     for n, txin in enumerate(data['vin']):
         try:
             txin_data = rpc.getrawtransaction(txin['txid'], verbose=True)
-            data['vin'][n]['value'] =  txin_data['vout'][n]['value']
+            data['vin'][n]['value'] =  txin_data['vout'][txin['vout']]['value']
             # data['vin'][n]['address'] =  txin_data['vout'][n]['scriptPubKey']['addresses'][0]
-            data['vin'][n]['scriptPubKey'] = txin_data['vout'][n]['scriptPubKey']
+            data['vin'][n]['scriptPubKey'] = txin_data['vout'][txin['vout']]['scriptPubKey']
         except:
             data['vin'][n]['value'] = None
             data['vin'][n]['scriptPubKey'] = None
 
     for n, txout in enumerate(data['vout']):
         data['vout'][n]['spent'] = False if rpc.gettxout(txid, n) else True
+
     return render_template(
         'txn.html',
         data=data
@@ -137,3 +144,28 @@ def page_not_found(e):
 @app.template_filter('ctime')
 def timectime(s):
     return datetime.fromtimestamp(s)
+
+### Auxiliary Functions ###
+def build_genesis_txn():
+    genesis_block_hash = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+    block_data = rpc.getblock(genesis_block_hash, verbosity=2)
+
+    # build txn data from genesis block data
+    data = dict()
+    data['txid'] = block_data['tx'][0]['txid']
+    data['hash'] = block_data['tx'][0]['hash']
+    data['version'] = block_data['tx'][0]['version']
+    data['size'] = block_data['tx'][0]['size']
+    data['version'] = block_data['tx'][0]['version']
+    data['vsize'] = block_data['tx'][0]['vsize']
+    data['weight'] = block_data['tx'][0]['weight']
+    data['locktime'] = block_data['tx'][0]['locktime']
+    data['vin'] = block_data['tx'][0]['vin']
+    data['vout'] = block_data['tx'][0]['vout']
+    data['hex'] = block_data['tx'][0]['hex']
+    data['blockhash'] = block_data['hash']
+    data['confirmations'] = block_data['confirmations']
+    data['time'] = block_data['time']
+    data['blocktime'] = block_data['time']
+
+    return data
